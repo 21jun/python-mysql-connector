e228d9beb01039eadbf50e42bd34a8dc47a6a31f
21jun <21jun7654@gmail.com>
2020-03-03 23:52:24 +0900

    add: save and load

--- find_features.py
+++ find_features.py
@@ -4,8 +4,10 @@ import pandas as pd
 import numpy as np
 import re
 import nltk
+import pickle
 
 from collections import Counter
+from pathlib import Path
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
@@ -29,24 +31,61 @@ class WordFrequency:
         self.comments = self.db.to_df(self.SQL)
         self.games = {}
         self.gameNames = self.comments['gameName'].unique()
+        self.appids = self.comments
         self.stop_words = stopwords.words('english')
 
-        for gameName in self.gameNames:
+        g = self.comments.groupby(['appid', 'gameName']).size().reset_index()
+        appids, gameNames = g['appid'].tolist(), g['gameName'].tolist()
+
+        for appid, gameName in zip(appids, gameNames):
             text = self.comments[self.comments['gameName'] == gameName]['text']
             text = text.tolist()
             text = [clean_text(t) for t in text]
             text = ' '.join(text)
-            self.games[gameName] = text
+            # print(appid)
+            # print(text)
+            self.games[gameName] = {'appid': appid, 'text': text}
+        # for gameName in self.gameNames:
+        #     text = self.comments[self.comments['gameName'] == gameName]['text']
+        #     text = text.tolist()
+        #     text = [clean_text(t) for t in text]
+        #     text = ' '.join(text)
+        #     self.games[gameName] = text
 
     def get_word_count(self, gameName):
-        tokens = nltk.word_tokenize(self.games[gameName])
+        tokens = nltk.word_tokenize(self.games[gameName]['text'])
         tokens = [w for w in tokens if not w in self.stop_words]
         word_count = Counter(tokens)
         return {k: v for k, v in sorted(word_count.items(), key=lambda item: item[1], reverse=True)}
 
+    def save_word_count(self, gameName, word_count, path):
+        path = Path(path)
+        appid = self.games[gameName]['appid']
+        fileName = gameName + '_' + str(appid) + '.pickle'
+        # fileName = gameName+'.pickle'
+        with open(path/fileName, 'wb+') as f:
+            pickle.dump(word_count, f, pickle.HIGHEST_PROTOCOL)
+
+    def load_word_count(self, gameName, path):
+        # path = Path(path).glob("**/*.pickle")
+        # files = [x for x in path if x.is_file()]
+        path = Path(path)
+        appid = self.games[gameName]['appid']
+        fileName = gameName + '_' + str(appid) + '.pickle'
+
+        with open(path/fileName, 'rb') as f:
+            word_counts = pickle.load(f)
+
+        return word_counts
+
 
 if __name__ == '__main__':
 
+    gameName = "Dota Underlords"
+    path = Path("word_counts")
+
     wf = WordFrequency()
-    cnt = wf.get_word_count("Neon Boost")
+    cnt = wf.get_word_count(gameName)
+    wf.save_word_count(gameName, cnt, "word_counts")
+    # cnt = wf.load_word_count(gameName, path)
     print(cnt)
